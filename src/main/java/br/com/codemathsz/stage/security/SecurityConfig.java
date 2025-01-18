e22a9d26673d2d48b0bebe33e13237854bdc0159
codemathsz <mo284891@gmail.com>
2025-01-18 17:35:43 -0300

    feat: config security and filter routes

---
+++ src/main/java/br/com/codemathsz/stage/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package br.com.codemathsz.stage.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/users/").permitAll()
+                .requestMatchers("/users/auth").permitAll()
+                ;
+                auth.anyRequest().authenticated();
+            }
+        );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
