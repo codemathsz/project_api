92c9a4d7385704ef2a484b8c982aa77b64d9af7d
codemathsz <mo284891@gmail.com>
2025-01-18 15:39:02 -0300

    feat: initial commit

---
+++ src/test/java/br/com/codemathsz/stage/StageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codemathsz.stage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
