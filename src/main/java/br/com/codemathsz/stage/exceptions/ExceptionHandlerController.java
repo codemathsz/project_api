f48142d297519538cf370ff7da0e15847f9b2bae
codemathsz <mo284891@gmail.com>
2025-01-19 14:01:52 -0300

    feat: clear ExceptionHandlerController imports

--- src/main/java/br/com/codemathsz/stage/exceptions/ExceptionHandlerController.java
+++ src/main/java/br/com/codemathsz/stage/exceptions/ExceptionHandlerController.java
@@ -1,18 +1,12 @@
 package br.com.codemathsz.stage.exceptions;
 
 import br.com.codemathsz.stage.dtos.ErrorMessageDTO;
-import jakarta.validation.ConstraintViolationException;
 import org.postgresql.util.PSQLException;
-import org.springframework.context.MessageSource;
-import org.springframework.context.MessageSourceResolvable;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
